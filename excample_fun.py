0317025954e5836e048a3254e7f820337b41e4c5
ramoji4b5 <ramojirao999@gmail.com>
2022-07-18 06:08:18 +0530

    fibonacci series generation.

---
+++ excample_fun.py
@@ -0,0 +1,14 @@
+# fibonacci series generation.
+# @author : ramoi
+def fib_gen():
+  a,b=0,1
+  count = 0
+  n = input("enter the no of terms ins fibonacci series generation :\n")
+  print(type(n))
+  while count < int(n):
+    #yield a
+    print('---', a)
+    a,b=b,a+b
+    count = count +1
+
+fs=fib_gen()
